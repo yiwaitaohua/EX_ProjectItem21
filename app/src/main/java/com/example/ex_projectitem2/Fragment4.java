65778e7492f6e4e5e7c920c31bf9aca43028bd4c
admin <18649760295@163.com>
2016-05-10 01:09:29 +0800

    0509个人中心

--- app/src/main/java/com/example/ex_projectitem2/Fragment4.java
+++ app/src/main/java/com/example/ex_projectitem2/Fragment4.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -12,14 +15,44 @@ import android.view.ViewGroup;
  */
 public class Fragment4 extends Fragment {
 
+
+	private String[] geren_up= new String[]{"正在进行","已经揭晓","中奖记录","我的收藏"};
 	public Fragment4() {
 		// Required empty public constructor
 	}
-
+	View inflate;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_fragment4, container, false);
+		inflate = inflater.inflate(R.layout.fragment_fragment4, container, false);
+		ListView listView = (ListView)inflate.findViewById(R.id.listUp_Personal_Center);
+		return inflate;
+	}
+
+	class MyPersonalCenterAdapter extends BaseAdapter{
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View inflate2 = inflate.inflate(getContext(), R.layout.personal_center_item, null);
+			TextView tv_geren_up = (TextView) inflate2.findViewById(R.id.tv_geren_up);
+			tv_geren_up.setText(geren_up[position].toString());
+			return inflate2;
+		}
+
+		@Override
+		public int getCount() {
+			return 4;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
 	}
 
 }
